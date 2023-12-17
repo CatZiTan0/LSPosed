d819622cdd9c69341f3c7ceb6dc351adf407bff4
CatZiTan0 <111762567+CatZiTan0@users.noreply.github.com>
2023-12-17 18:12:27 +0800

    Update XposedBridge.java

--- core/src/main/java/de/robv/android/xposed/XposedBridge.java
+++ core/src/main/java/de/robv/android/xposed/XposedBridge.java
@@ -145,7 +145,7 @@ public final class XposedBridge {
      * @param text The log message.
      */
     public synchronized static void log(String text) {
-        Log.i(TAG, text);
+        Log.e(TAG, text);
     }
 
     /**
